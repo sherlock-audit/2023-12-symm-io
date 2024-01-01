00ba1348b601b0c463cd77314b0462cbedb4ba5e
sherlock-admin <dev@sherlock.xyz>
2024-01-01 12:59:08 +0000

    Copy solver-vaults @ 4bdebbecb66e29ac18e5a5c9eda42e4cb44cdd65

---
+++ solver-vaults/contracts/SolverVaultToken.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: SYMM-Core-Business-Source-License-1.1
+// This contract is licensed under the SYMM Core Business Source License 1.1
+// Copyright (c) 2023 Symmetry Labs AG
+// For more information, see https://docs.symm.io/legal-disclaimer/license
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/extensions/AccessControlEnumerable.sol";
+
+contract SolverVaultToken is ERC20, ERC20Burnable, AccessControlEnumerable {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    constructor() ERC20("SolverVaultToken", "SVT") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
+}
